134dd0bbfe9aa979b637cf3d8f199e602d7df0f4
Rahul Jain <42921278+jainr3@users.noreply.github.com>
2023-05-11 17:10:00 -0400

    Update firebase.js
    removed API keys

--- react-app/src/firebase.js
+++ react-app/src/firebase.js
@@ -8,12 +8,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyARVQ6ErRCEE5Iq-cvv9HD3nKaB95_jn48",
-    authDomain: "bigco-studio.firebaseapp.com",
+    apiKey: "",
+    authDomain: "",
     projectId: "bigco-studio",
-    storageBucket: "bigco-studio.appspot.com",
-    messagingSenderId: "723040240101",
-    appId: "1:723040240101:web:ad93a335c61e9436336f0c"
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 // Initialize Firebase
